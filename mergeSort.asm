e8464f13c1965954d2c684a7c76bc5f2692f4e8e
Kareem Abdullah <10812022100918@stud.cu.edu.eg>
2025-03-11 14:25:19 +0200

    Merge sort and min & max for an array

---
+++ mergeSort.asm
@@ -0,0 +1,103 @@
+;include 'emu8086.inc'
+org 100h
+
+  
+;mov bl, data[si] 
+;mov al, data[si]
+;
+;mov max, al
+;mov min, bl
+;
+;
+;mov cx, 4
+;        
+;inc si
+;
+;l1:        
+;    mov dl, data[si]
+;    cmp dl, max   
+;    jg a ; for unsigned and for signed use ja
+;    jmp skip
+;    a:
+;        mov max, dl
+;    
+;    skip:
+;        cmp dl, min
+;        jl b ; dl < min for unsigned and for signed use jb
+;        jmp break
+;    
+;    b:
+;        mov min, dl
+;    
+;    break:
+;    inc si
+;loop l1
+;
+;
+;ret 
+;data db -5, 4, -2, -7, 1
+;min db ?
+;max db ?
+
+;lea si, arr1
+;lea di, arr2
+
+mov cx, 3 
+mov si, 0
+mov di, 0
+mov bp, 0
+
+;mov al, [si]
+;mov bl [di]
+;
+;cmp al, bl
+;jb o
+;
+;o:
+;    mov [arr] 
+
+l1:  
+
+    mov al, arr1[si]
+    mov bl, arr2[di]
+    cmp al, bl
+    jb b
+    ;jmp a
+    a:
+       mov arr[bp], bl
+       inc di
+       jmp back
+       
+    b:  
+       mov arr[bp], al 
+       inc si
+       jmp back 
+    
+    
+    eq1:
+        mov arr[bp], bl
+        inc di
+        jmp skip
+        
+    eq2:
+        mov arr[bp], al
+        inc si
+        jmp skip
+        
+    back:  
+        inc bp
+        cmp si, 2
+        je eq1
+    
+        cmp di, 2
+        je eq2
+    
+    skip:    
+                
+    
+loop l1
+
+ret
+arr1 db 2, 9
+arr2 db 7, 8
+arr db 4h dup(?)
